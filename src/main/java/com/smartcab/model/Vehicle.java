b66c378fa2dbe0205b78cecc86a978047fe848de
Mahesh Jain (V) <mahesh.jain@apple.com>
2015-07-15 20:44:22 -0700

    first class

---
+++ src/main/java/com/smartcab/model/Vehicle.java
@@ -0,0 +1,38 @@
+package com.smartcab.model;
+
+public class Vehicle {
+
+	String vehicleId;
+	String vehicleType;
+
+	/**
+	 * @return the vehicleId
+	 */
+	public String getVehicleId() {
+		return vehicleId;
+	}
+
+	/**
+	 * @param vehicleId
+	 *            the vehicleId to set
+	 */
+	public void setVehicleId(String vehicleId) {
+		this.vehicleId = vehicleId;
+	}
+
+	/**
+	 * @return the vehicleType
+	 */
+	public String getVehicleType() {
+		return vehicleType;
+	}
+
+	/**
+	 * @param vehicleType
+	 *            the vehicleType to set
+	 */
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+}
