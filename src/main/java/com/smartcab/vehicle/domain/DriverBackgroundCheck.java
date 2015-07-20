e4a6f304ff0c6fa0e69a5ceec30eab1b36ded8f1
souachar <souachar@localhost>
2015-07-20 01:08:46 -0700

    uml

--- src/main/java/com/smartcab/vehicle/domain/DriverBackgroundCheck.java
+++ src/main/java/com/smartcab/vehicle/domain/DriverBackgroundCheck.java
@@ -1,15 +1,9 @@
 package com.smartcab.vehicle.domain;
 
 public class DriverBackgroundCheck extends Driver {
-	private int driverId;
 	private boolean isbgCheckdone;
 	private String agency;
-	/**
-	 * @return the driverId
-	 */
-	public int getDriverId() {
-		return driverId;
-	}
+
 	/**
 	 * @return the isbgCheckdone
 	 */
@@ -21,12 +15,6 @@ public class DriverBackgroundCheck extends Driver {
 	 */
 	public String getAgency() {
 		return agency;
-	}
-	/**
-	 * @param driverId the driverId to set
-	 */
-	public void setDriverId(int driverId) {
-		this.driverId = driverId;
 	}
 	/**
 	 * @param isbgCheckdone the isbgCheckdone to set
