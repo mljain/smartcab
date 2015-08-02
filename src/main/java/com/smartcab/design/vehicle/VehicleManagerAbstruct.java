8dc5333034b092db4e9b7670beb326a685023a0a
souachar <souachar@localhost>
2015-08-02 14:32:19 -0700

    Comiit Car

--- src/main/java/com/smartcab/design/vehicle/VehicleManagerAbstruct.java
+++ src/main/java/com/smartcab/design/vehicle/VehicleManagerAbstruct.java
@@ -1,18 +1,17 @@
 package com.smartcab.design.vehicle;
 
+import java.util.HashMap;
+
+import com.smartcab.vehicle.domain.Vehicle;
+
 public abstract class VehicleManagerAbstruct implements VehicleManager {
-
+	public HashMap<Integer, Vehicle> vehicleInventory = new HashMap<Integer, Vehicle>();
 	static VehicleManager pm;
-
 	public static VehicleManager getInstance() {
 		// if(pm==null) {
 		// pm = new VehicleManager();
 		// }
 		return pm;
 	}
-
-	private Car car = new Car();
-	private Bus bus = new Bus();
-	private Suv suv = new Suv();
 
 }
