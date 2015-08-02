a3291e2407f703463d5d920e41c24e3aca6e542a
souachar <souachar@localhost>
2015-08-02 14:32:19 -0700

    Comiit Car

--- src/main/java/com/smartcab/design/vehicle/Car.java
+++ src/main/java/com/smartcab/design/vehicle/Car.java
@@ -1,37 +1,56 @@
 package com.smartcab.design.vehicle;
 
+import java.util.Map.Entry;
+
 import com.smartcab.vehicle.domain.Vehicle;
 
 public class Car extends VehicleManagerAbstruct {
 
 	public boolean addVehicle(Vehicle vehicle) {
-		// TODO Auto-generated method stub
+		try {
+			vehicleInventory.put(vehicle.getVehicleId(), vehicle);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	public boolean removeVehicle(Vehicle vehicle) {
-		// TODO Auto-generated method stub
-		return false;
+		vehicleInventory.remove(vehicle.getVehicleId());
+		return true;
 	}
 
 	public boolean updateVehicle(Vehicle vehicle) {
-		// TODO Auto-generated method stub
+		for (Entry<Integer, Vehicle> vehicle1 : vehicleInventory.entrySet()) {
+			Vehicle vehicle2 = vehicle1.getValue();
+			if (vehicle2.getVehicleId() == vehicle.getVehicleId()) {
+				vehicleInventory.put(vehicle.getVehicleId(), vehicle);
+				return true;
+			} else {
+				return false;
+			}
+
+		}
 		return false;
 	}
 
 	public Vehicle getVehicle(int vehicleId) {
-		// TODO Auto-generated method stub
+		if (vehicleInventory.get(vehicleId) != null) {
+			return vehicleInventory.get(vehicleId);
+
+		}
 		return null;
 	}
 
 	public void maintainVehicle(String operation, Vehicle vehicle) {
 		// TODO Auto-generated method stub
-		if(operation.equals("add")){
+		if (operation.equals("add")) {
 			addVehicle(vehicle);
-		}else if(operation.equals("delete")){
+		} else if (operation.equals("delete")) {
 			removeVehicle(vehicle);
 		}
-		
+
 	}
 
 }
